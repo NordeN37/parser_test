dd97d923eb031f495c40a8a97e26fe9c43130c0d
Sergey Bazhenov <bazhenovsm@gmail.com>
2022-09-02 23:34:50 +0300

    fixed parser

--- internal/models/parser_result.go
+++ internal/models/parser_result.go
@@ -3,7 +3,12 @@ package models
 import "time"
 
 type ParserResult struct {
-	Date        time.Time              `bson:"date"`
-	Name        string                 `bson:"name"`
-	Description map[string]interface{} `bson:"description"`
+	Date        time.Time               `bson:"date"`
+	Name        string                  `bson:"name"`
+	Description *[]ParseSelectionResult `bson:"description"`
+}
+
+type ParseSelectionResult struct {
+	Value      *string
+	FoundValue *[]ParseSelectionResult
 }
